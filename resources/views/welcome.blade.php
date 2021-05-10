37a5a261b562fd023e7c4c2edaf95e9ee6d024f6
hermessharif <hermessharif1400@gmail.com>
2021-05-10 16:02:45 +0430

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,22 @@
 @extends('layouts.app')
 
 @section('content')
-    @guest
-        <welcome-component></welcome-component>
-    @else
-        <welcome-component user="{{auth()->user()}}"></welcome-component>
-    @endguest
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <div class="text-center"><img src="/storage/logo.jpg" alt=""></div>
+                        <div class="text-center">در دست ساخت</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+{{--    @guest--}}
+{{--        <welcome-component></welcome-component>--}}
+{{--    @else--}}
+{{--        <welcome-component user="{{auth()->user()}}"></welcome-component>--}}
+{{--    @endguest--}}
 @endsection
